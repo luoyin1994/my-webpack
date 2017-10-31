66b3dab3c920ef0e513b8f1e17a6608683069875
luoyin <luoyin1994@icloud.com>
2017-10-31 16:40:54 +0800

    using extractTextPlugin to extract css

--- config/webpack/webpack.conf.base.js
+++ config/webpack/webpack.conf.base.js
@@ -2,15 +2,48 @@ var path = require('path');
 
 var webpackConfig = require('./webpack.config');
 
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+
 var config = {
     context: path.resolve(webpackConfig.rootDirPath, 'src'),
     entry  : {
-        main  : './main.js',
+        main: './main.js',
     },
     output : {
         filename     : '[name].js',
         chunkFilename: '[name]-[id].js',
     },
+    module : {
+        rules: [
+            {
+                test: /\.css$/,
+                use : ExtractTextPlugin.extract({
+                    fallback: 'style-loader',
+                    use     : 'css-loader',
+                }),
+                // use: [
+                //     'style-loader',
+                //     'css-loader'
+                // ]
+            },
+            {
+                test: /\.(png|svg|jpg|gif)$/,
+                use : [
+                    'file-loader',
+                ],
+            },
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/,
+                use : [
+                    'file-loader',
+                ],
+            },
+
+        ],
+    },
+    plugins: [
+        new ExtractTextPlugin('css/[name].css'),
+    ],
 };
 
 module.exports = config;
--- src/main.js
+++ src/main.js
@@ -1,7 +1,14 @@
 import './a.js';
 import './b.js';
+
 import _ from 'lodash';
 import utils from './js/utils/utils';
+
+import './css/main.css';
+
+// json默认被转化为对象
+import testJson from '../mock/test.json';
+console.log(testJson);
 
 console.log('I am File!');
 
@@ -9,4 +16,5 @@ console.log(_.join(['a', 'b', 'c'], '~'));
 
 utils.appendDom('<div>I am Div!</div>', 'body');
 utils.appendDom('<p>I am P!</p>', 'body');
+utils.appendDom('<img src="img/bird.jpg" height="200">', 'body');
 
