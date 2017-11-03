268d2080ec89f092368ef9f2fbe300a31297bbd0
luoyin <luoyin1994@icloud.com>
2017-11-03 13:05:57 +0800

    modified listen console log

--- src/node/app.js
+++ src/node/app.js
@@ -1,14 +1,16 @@
 const PORT   = 3000;
+const HOST   = 'localhost';
 const config = require('./config/config');
 
 const path = require('path');
 
-const Koa   = require('koa');
-const app   = new Koa();
+const Koa = require('koa');
+const app = new Koa();
+
 const serve = require('koa-static');
 
 // set public directory
 app.use(serve(path.resolve(config.prodDirPath)));
 
 app.listen(PORT);
-console.log(`listening on port ${PORT}`);
+console.log(`listening on http://${HOST}:${PORT}.`);
