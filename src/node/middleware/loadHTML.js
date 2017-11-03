2757958dc654a2db593297e9412136a9815d8dbd
luoyin <luoyin1994@icloud.com>
2017-11-03 15:13:43 +0800

    replace uglifyJSPlugin

--- src/node/middleware/loadHTML.js
+++
@@ -1,6 +0,0 @@
-module.exports = function(dir) {
-    const fs   = require('fs');
-    const path = require('path');
-    let html   = fs.readFileSync(path.resolve(__dirname, '../', dir), 'utf-8');
-    return html;
-};
