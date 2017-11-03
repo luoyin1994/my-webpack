7b35546eaea7425edb2afa50fdc644d85b0969de
luoyin <luoyin1994@icloud.com>
2017-11-03 12:44:39 +0800

    modified src/node/app.js

--- src/node/app.js
+++ src/node/app.js
@@ -1,5 +1,5 @@
 const PORT   = 3000;
-const config = require('config');
+const config = require('./config/config');
 
 const path = require('path');
 
@@ -7,6 +7,7 @@ const Koa   = require('koa');
 const app   = new Koa();
 const serve = require('koa-static');
 
+// set public directory
 app.use(serve(path.resolve(config.prodDirPath)));
 
 app.listen(PORT);
